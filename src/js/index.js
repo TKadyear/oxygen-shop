35c125a0a04ac6b05769afc16735cb33e6cda7f2
TKadyear <tamarakadyear@gmail.com>
2022-03-28 22:17:19 +0200

    Added function ScrollUp

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
+const ScrollUp = () => {
+  const currentScroll = document.body.scrollTop
+  console.log(currentScroll)
+  // window.requestAnimationFrame()
+}
+
+const btnScroll = document.querySelector(".btn__scroll_up");
+btnScroll.addEventListener("click", () => {
+  setTimeout(() => window.scrollTo(0, 0), 200)
+  ScrollUp()
+})
+
 window.addEventListener("scroll", () => {
   const percentageScroll = (window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight)
   document.querySelector(".percentage-scroller").style.width = percentageScroll + "%"
