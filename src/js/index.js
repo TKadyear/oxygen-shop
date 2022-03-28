e7470c9a8c238baf2e45e1571d1659d936391306
TKadyear <tamarakadyear@gmail.com>
2022-03-28 13:29:30 +0200

    Added a break for pop up when is 25%

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
+const createPopUp = () => {
+  alert("son 5s o 25%")
+}
 window.addEventListener("scroll", () => {
-  const percentageScroll = (window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight)
+  const percentageScroll = Math.trunc((window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight))
   document.querySelector(".percentage-scroller").style.width = percentageScroll + "%"
+  console.log(percentageScroll)
+  if (percentageScroll === 25) {
+    createPopUp();
+  }
 })
