4962ee0b30f8dc386546735ca46236a282847ad0
TKadyear <tamarakadyear@gmail.com>
2022-03-29 11:16:44 +0200

    Added createPopUp and the way to close the pop up

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,58 @@
-const createPopUp = () => {
-  if (!document.querySelector(".info__newsletter__container")) {
+const blockScrollBody = (block) => block ? document.body.classList.add("block__scroll") : document.body.classList.remove("block__scroll")
+const addEventsToNewsletter = () => {
+  const bgNewsletter = document.querySelector("#newsletter__bg")
+  const closeNewsletter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    blockScrollBody(false);
+    bgNewsletter.remove()
+  }
+  bgNewsletter.addEventListener("click", closeNewsletter)
 
+  document.querySelector(".newsletter__btn--exit").addEventListener("click", (e) => {
+    closeNewsletter(e);
+  })
+  document.querySelector(".info__newsletter__btn__close").addEventListener("click", (e) => {
+    closeNewsletter(e);
+  })
+}
+const createPopUpNewsletter = () => {
+  if (!document.querySelector(".info__newsletter__container")) {
     const template = /*html */`
-    <div class="info__newsletter__container">
+    <div id="newsletter__bg" class="info__newsletter__container">
       <div class="info__newsletter">
-        <div class="btn__close"></div>
+        <div class="info__newsletter__btn__close">
+          <svg width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.492 12.501 12.494 1.5m0 11.002L1.492 1.5" stroke="#07ACE6" stroke-width="2"/>
+          </svg>
+        </div>
         <h6 class="info__newsletter_h6">Don't miss any updates</h6>
         <p class="info__newsletter_p">Get the lastest content and best deals in your inbox every day!</p>
-        <label class="info__newsletter__label">Email<input type="email" class="info__newsletter__input" name="newsletter__email" required></label>
-
-        <label class="info__newsletter__label" id="data-protection" for="data-protection">
-        <input type="checkbox" name="data-protection" required>
-        <p> I hereby give consent for my personal data included in my application to be processed for the purposes of
-          the
-          recruitment process under the European Parliament’s and Council of the European Union Regulation on the
-          Protection of Natural Persons as of 27 April 2016, with regard to the processing of personal data and on the
-          free movement of such data, and repealing Directive 95/46/EC (Data Protection Directive)
-        </p>
-      </label>
+        <form>
+          <label class="info__newsletter__label">Email<input type="email" class="info__newsletter__input" name="newsletter__email" required></label>
+          <label class="info__newsletter__label info__newsletter__label--checkbox" id="data-protection" for="data-protection">
+            <input class="info__newsletter__checkbox" type="checkbox" name="data-protection" required>
+            <p class="info__newsletter__label__p"> I hereby give consent for my personal data included in my application to be processed for the purposes of
+            the
+            recruitment process under the European Parliament’s and Council of the European Union Regulation on the
+            Protection of Natural Persons as of 27 April 2016, with regard to the processing of personal data and on the
+            free movement of such data, and repealing Directive 95/46/EC (Data Protection Directive)
+            </p>
+        </label>
+        <div class="info__newsletter__form__container__btn">
+        <button  class="newsletter__btn--exit">I'm not interesed</button>
         <button  class="newsletter__btn btn-primary">Send</button>
+        </div>
+        </form>
       </div>
     </div>
     `
-    const range = new Range();
-    const documentTemplate = range.createContextualFragment(template)
-    const body = document.body
-    body.classList.add("block__scroll")
-
-    console.log(documentTemplate.querySelector(".newsletter__btn"))
-    body.insertAdjacentHTML("beforeend", template)
-    const bgNewsletter = documentTemplate.querySelector(".info__newsletter__container")
-    bgNewsletter.addEventListener("click", (e) => {
-      e.preventDefault()
-      console.log(bgNewsletter)
-    })
-    documentTemplate.querySelector(".newsletter__btn").addEventListener("click", () => {
-      console.log("boton")
-    })
+    blockScrollBody(true);
+    document.body.insertAdjacentHTML("beforeend", template)
+    addEventsToNewsletter();
   }
 }
+
 
 const scrollToHeader = () => {
   const currentScroll = window.scrollY
@@ -57,9 +71,9 @@ window.addEventListener("scroll", () => {
   const percentageScroll = Math.trunc((window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight))
   document.querySelector(".percentage-scroller").style.width = percentageScroll + "%"
   if (percentageScroll === 25) {
-    createPopUp();
+    createPopUpNewsletter();
   }
 })
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(createPopUp, 5000)
+  setTimeout(createPopUpNewsletter, 5000)
 })
