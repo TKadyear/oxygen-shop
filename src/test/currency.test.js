a435d70bb1eca6ef8c9e2575be4d0a7afe528660
TKadyear <tamarakadyear@gmail.com>
2022-03-31 12:35:41 +0200

    Uninstall jest

--- src/test/currency.test.js
+++
@@ -1,7 +0,0 @@
-const changeCurrency = require("./currency")
-
-describe("Cambio de moneda de Dolares $ a Libras o Euros", () => {
-  test("Cambio a Euros", () => {
-    expect(changeCurrency("")).toBe("something")
-  })
-})
