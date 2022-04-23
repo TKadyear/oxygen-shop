0ce9111deb9bfb54d6a0afae5ea82511915b6381
TKadyear <tamarakadyear@gmail.com>
2022-04-23 17:26:08 +0200

    Encapsulation of the CSS Classes

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -4,17 +4,23 @@ export class SliderID {
     this.images = this.containerImages.children;
     this.listImg = Array.from(this.images);
     this.listButtons = [];
-    this.imageStyleClass = "slide__img";
-    this.imageActiveClass = "slide__img--active";
-    this.btnContainerStyleClass = "slide__btn__container";
-    this.btnStyleClass = "slide__btn";
-    this.btnActiveClass = "slide__btn--active";
+    this.style = {
+      image: {
+        base: "slide__img",
+        active: "slide__img--active"
+      },
+      btn: {
+        container: "slide__btn__container",
+        base: "slide__btn",
+        active: "slide__btn--active"
+      }
+    }
     this.activeIndex = 0;
     this.intervalSlide;
     this.initialization();
   }
   addImageClasses() {
-    this.listImg.forEach(img => img.classList.add(this.imageStyleClass));
+    this.listImg.forEach(img => img.classList.add(this.style.image.base));
   }
   listenerBtn() {
     this.listButtons.forEach((btn, position) => {
@@ -28,24 +34,24 @@ export class SliderID {
   }
   generateBtn() {
     const buttons = this.listImg.map(() => /*html */
-      `<button class=${this.btnStyleClass}></button>`
+      `<button class=${this.style.btn.base}></button>`
     )
     const containerBtn = /* html */`
-    <div class=${this.btnContainerStyleClass}>${buttons.join("")}</div>
+    <div class=${this.style.btn.container}>${buttons.join("")}</div>
     `
     const range = document.createRange();
     const DocumentContainerBtn = range.createContextualFragment(containerBtn);
-    this.listButtons = DocumentContainerBtn.querySelectorAll("." + this.btnStyleClass)
+    this.listButtons = DocumentContainerBtn.querySelectorAll("." + this.style.btn.base)
     this.listenerBtn();
     this.containerImages.appendChild(DocumentContainerBtn);
   }
   removeClass() {
-    this.images[this.activeIndex].classList.remove(this.imageActiveClass);
-    this.listButtons[this.activeIndex].classList.remove(this.btnActiveClass);
+    this.images[this.activeIndex].classList.remove(this.style.image.active);
+    this.listButtons[this.activeIndex].classList.remove(this.style.btn.active);
   }
   addClass() {
-    this.images[this.activeIndex].classList.add(this.imageActiveClass);
-    this.listButtons[this.activeIndex].classList.add(this.btnActiveClass);
+    this.images[this.activeIndex].classList.add(this.style.image.active);
+    this.listButtons[this.activeIndex].classList.add(this.style.btn.active);
   }
   startInterval() {
     clearInterval(this.intervalSlide);
